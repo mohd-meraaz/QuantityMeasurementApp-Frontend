58348a387d930325579c8d58e6ed631f0b8935c7
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-04-04 14:17:53 +0530

    [MohdMeraaz] : Added enviromnent variables of backend

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,6 @@
-
 export const environment = {
   production: true,
-  gatewayUrl: 'http://localhost:8080', // Your real server
-  googleClientId: 'your-client id',
-  googleRedirectUri: 'http://localhost:4200/auth'
+  gatewayUrl: 'https://quantitymeasurementapp-production-387c.up.railway.app',
+  googleClientId: '918583838409-p07mh53r3bbhm478nnce7g9kf2o2h863.apps.googleusercontent',
+  googleRedirectUri: 'https://quantity-measurement-app-frontend-ruby.vercel.app/auth'
 };
