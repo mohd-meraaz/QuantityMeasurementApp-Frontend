1aca3f390b06575caec6f0f9bf5062c0dd588ff5
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-04-09 12:12:01 +0530

    [MohdMeraaz] : Added common Name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'https://api.yourdomain.com', // Your real server
-  googleClientId: '880694243289-hmeu169t5g1etinlqr8td1cv9crfb0gq.apps.googleusercontent.com',
-  googleRedirectUri: 'https://yourdomain.com/auth'
+  gatewayUrl: 'GATEWAY_URL',
+  googleClientId: 'GOOGLE_CLIENT_ID',
+  googleRedirectUri: 'GOOGLE_REDIRECT_URI'
 };
